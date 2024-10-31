ad5d4b517ce7645bb23e1ac84c8ac984b771e60e
Ahmed Refai <momo3956248@gmail.com>
2024-10-31 03:27:07 +0300

    Company & Update Theme

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,14 +1,18 @@
-let darkMode = document.querySelector('.input-mode');
-let html = document.querySelector('html');
-console.log(html);
-darkMode.addEventListener('click', function () {
-  if (darkMode.getAttribute('data-toggle') === 'false') {
-    darkMode.setAttribute('data-toggle', 'true');
-    html.setAttribute('class', 'dark');
-    console.log(darkMode);
+let toggleThemeIcon = document.querySelector('.input-mode');
+let theme = localStorage.getItem('theme');
+console.log(theme);
+if (theme === 'dark') {
+  document.documentElement.classList.add('dark');
+  toggleThemeIcon.setAttribute('checked', true);
+}
+toggleThemeIcon.addEventListener('change', function (e) {
+  if (theme === 'light') {
+    document.documentElement.classList.add('dark');
+    theme = 'dark';
+    localStorage.setItem('theme', theme);
   } else {
-    darkMode.setAttribute('data-toggle', 'false');
-    html.setAttribute('class', 'light');
-    console.log(darkMode);
+    theme = 'light';
+    localStorage.setItem('theme', theme);
+    document.documentElement.classList.remove('dark');
   }
 });
